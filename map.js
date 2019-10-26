1bf46cde5f9d767edf16f0487bb43e480dd84976
Toalba <tb@kleinundpartner.at>
2019-10-26 12:28:30 +0200

    map-linz

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
 window.addEventListener("DOMContentLoaded",function(){
-    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+    var mymap = L.map('mapid').setView([48.3003,14.2954], 13);
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar', attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'}).addTo(mymap);
 })
