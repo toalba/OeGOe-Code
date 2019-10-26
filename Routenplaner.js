b06f6f7c50abdc54495533d77a96087d607c38c4
leginger02 <gioaluger@gmail.com>
2019-10-26 18:34:54 +0200

    ä

---
+++ Routenplaner.js
@@ -0,0 +1,5 @@
+window.addEventListener("DOMContentLoaded",function(){
+    document.getElementById("Carsharing").addEventListener("click",function(){
+       window.open("https://www.blablacar.de/#comuto_cmkt=AT_CARPOOLING_MFG-Redirect");
+    });
+    });
