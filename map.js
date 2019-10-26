49f0e3d49327573f7a1aa957d613b9908139f79c
Toalba <tb@kleinundpartner.at>
2019-10-26 23:41:45 +0200

    fahrradverleih linz

--- map.js
+++ map.js
@@ -21,13 +21,13 @@ function do_something(latitude,longitude){
     iconUrl: 'Pins/greenleavest.png',
     iconSize: [30, 93.3],
 });
-  var Fahradgrün = L.icon({
-    iconUrl: 'Pins/Fahreadgrün.png',
+  var Fahrradgrün = L.icon({
+    iconUrl: 'Pins/Fahrradgrün.png',
     iconSize: [30,30],
   });
   
   var marker = L.marker([latitude,longitude],{icon: stblatt}).addTo(mymap);
-  var fahradverllinz = L.marker([14.290217,48.306973], {icon: Fahrradgrün})
+  var fahradverllinz = L.marker([48.306973,14.290217], {icon: Fahrradgrün}).addTo(mymap);
 
   mymap.addLayer(createoverpasslayer('node["public_transport"="stop_position"]["bus"="yes"]({{bbox}});out body;>;out skel qt;','Pins/BusseroterPin.png',latitude,longitude));
   mymap.addLayer(createoverpasslayer('node["public_transport"="stop_position"]["tram"="yes"]({{bbox}});out body;>;out skel qt;','Pins/StrassenbahnorangerPin.png',latitude,longitude));
